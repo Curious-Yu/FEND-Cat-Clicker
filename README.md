ecd9142a7d0ddfdad53e4059e619fcba0ed9e3f0
WYCodeBook <wyu10amy@hotmail.com>
2018-11-02 16:32:42 -0700

    step two of the cat clicker

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # FEND-Cat-Clicker
+
+This project will record the change of a project over time.
+
+# Project Requirements for Cat Clicker
+
+## Step 1:
+
+Visuals
+
+* The application should display a picture of a cat and a number of clicks.
+* The specifics of the layout do not matter, so style it however you'd like.
+
+Interaction
+
+* The number of clicks should increment when the cat picture is clicked.
+
+## Step 2:
+
+Visuals
+
+* The application should display two cats. Each cat includes
+  * the cat's name
+  * a picture of the cat
+  * text showing the number of clicks
+
+* The specifics of the layout do not matter, so style it however you'd like.
+
+Interaction
+
+* The number of clicks should increment when each cat picture is clicked.
--- catclicker.js
+++ catclicker.js
@@ -1,8 +1,20 @@
-var click = 0;
+var click1 = 0;
+var click2 = 0;
 
-document.getElementById("clickCat").addEventListener("click", counterFunction, true);
+// event listener for cat #1 //
+document.getElementById("clickCat1").addEventListener("click", counterFunction1, true);
 
-function counterFunction() {
-  click++;
-  document.getElementById("clickCounter").innerHTML = "Cat Clicking Counter: " + click;
+// function for the click counter //
+function counterFunction1() {
+  click1++;
+  document.getElementById("clickCounter1").innerHTML = "Cat Clicking Counter: " + click1;
+};
+
+// event listener for cat #2 //
+document.getElementById("clickCat2").addEventListener("click", counterFunction2, true);
+
+// function for the click counter //
+function counterFunction2() {
+  click2++;
+  document.getElementById("clickCounter2").innerHTML = "Cat Clicking Counter: " + click2;
 };
