d33753a6555ddc0c7696e02bb606cfa835c49b8e
WYCodeBook <wyu10amy@hotmail.com>
2018-11-03 00:04:29 -0700

    save for now

--- catclicker.js
+++ catclicker.js
@@ -1,20 +1,14 @@
-var click1 = 0;
-var click2 = 0;
+var click = 0;
 
-// event listener for cat #1 //
-document.getElementById("clickCat1").addEventListener("click", counterFunction1, true);
+document.getElementById('sleepyCat').addEventListener("click",
+function catOpen(){
+  document.querySelector('#testing').classList.toggle('hide');
+});
 
-// function for the click counter //
-function counterFunction1() {
-  click1++;
-  document.getElementById("clickCounter1").innerHTML = "Cat Clicking Counter: " + click1;
-};
-
-// event listener for cat #2 //
-document.getElementById("clickCat2").addEventListener("click", counterFunction2, true);
+document.getElementById("catPic").addEventListener("click", counterFunction);
 
 // function for the click counter //
-function counterFunction2() {
-  click2++;
-  document.getElementById("clickCounter2").innerHTML = "Cat Clicking Counter: " + click2;
+function counterFunction() {
+  click++;
+  document.getElementById("clickCounter").innerHTML = "Cat Clicking Counter: " + click;
 };
